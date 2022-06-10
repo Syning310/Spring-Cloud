25e76d6fb965dbdcf21b379d18b41b793033b232
Syning <Syning310@outlook.com>
2022-06-10 12:39:19 +0800

    Bus 消息总线

---
+++ cloud-config-client-3366/src/main/java/com/syning/ConfigClient3366.java
@@ -0,0 +1,15 @@
+package com.syning;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ConfigClient3366 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClient3366.class, args);
+    }
+}
